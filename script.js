9e7a3533f1e2e4f28d787919e217d5f28c54457e
Ola Oladapo <olaoladapo007@gmail.com>
2022-06-24 22:07:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let addition = function (number1, number2) {
+  return number1 + number2;
+};
+
+let subtraction = function (number1, number2) {
+  return number1 - number2;
+};
+
+let division = function (number1, number2) {
+  return number1 / number2;
+};
+
+let multiplication = function (number1, number2) {
+  return number1 * number2;
+};
+
+console.log(addition(2, 3));
+console.log(subtraction(5, 3));
+console.log(division(15, 5));
+console.log(multiplication(2, 5));
